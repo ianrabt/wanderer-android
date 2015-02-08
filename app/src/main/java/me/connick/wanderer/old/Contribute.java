4af49e186fcf7bf5baa3bcb3823b94a71951f4d1
Connick <connickshields@gmail.com>
2015-02-08 02:56:21 -0800

    Things are work

--- app/src/main/java/me/connick/wanderer/old/Contribute.java
+++ app/src/main/java/me/connick/wanderer/old/Contribute.java
@@ -1,18 +1,25 @@
-package me.connick.wanderer;
+package me.connick.wanderer.old;
 
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.View;
+
+import me.connick.wanderer.R;
 
 /**
  * Created by connick on 2/7/15.
  */
 public class Contribute extends Activity{
 
-    public static int disc;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_discover_map);
+    }
 
     public void discClicked(View view){
         //Switch Activities (tabs)
-        setContentView(disc);
+        setContentView(R.layout.activity_discover_map);
     }
 
     public void photoClicked(View view){
